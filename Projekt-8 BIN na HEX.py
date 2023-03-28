d5b70e74fcfbaedcabbda39fab127e56138b9120
Maj0rIT <128848154+Maj0rIT@users.noreply.github.com>
2023-03-28 23:01:46 +0200

    Add files via upload

---
+++ Projekt-8 BIN na HEX.py
@@ -0,0 +1,8 @@
+
+dwujkowy = input("Podaj liczbe w systemie dwójkowym: ")
+decimal = int(dwujkowy, 2)
+szestnastkowy = hex(decimal)
+
+print("Liczba w systemie szesnastkowym to: ", szestnastkowy)
+
+input()
